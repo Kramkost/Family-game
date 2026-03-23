803bca2956d53c39c0dab12de19a87fff65a2539
Kotenkoff <kotenkoff1203@mail.ru>
2026-03-23 20:14:42 +0300

    New Branch

---
+++ Assets/scripts/Kotenkoff/Monsters/TheBlindWeaver.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using Mirror;
+
+namespace Kotenkoff.Monsters
+{
+    public class TheBlindWeaver : NetworkBehaviour
+    {
+        private GameObject triggerObject;
+    }
+}
---
+++ Assets/scripts/Kotenkoff/Monsters/TriggerObject.cs
@@ -0,0 +1,14 @@
+using Mirror;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace Kotenkoff.Monsters
+{
+    public class TriggerObject : NetworkBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            
+        }
+    }
+}

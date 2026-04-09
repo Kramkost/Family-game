e31207cd3e1d13c97e6d18863318f379d832e3dd
Kramkost <145920439+Kramkost@users.noreply.github.com>
2026-04-09 12:21:30 +0500

    Added sounds in the game

--- Assets/scripts/FoodItem.cs
+++ Assets/scripts/FoodItem.cs
@@ -9,6 +9,8 @@ public class FoodItem : PickupableItem, IUsableItem
 {
     [Header("Food Stats")]
     [SerializeField] private float nutritionValue = 30f;
+
+    [SerializeField] private AudioClip EatSound;
     
 
     [Server]
@@ -17,11 +19,30 @@ public class FoodItem : PickupableItem, IUsableItem
        
         if (player.TryGetComponent(out PlayerStats stats))
         {
+
+        if (EatSound != null)
+        {
+            RpcPlayEatSound();
+        }
             
             stats.Eat(nutritionValue);
+
+
             
           
             player.DestroyHeldItem(); 
+        }
+    }
+
+
+        [ClientRpc]
+    private void RpcPlayEatSound()
+    {
+            
+        if (TryGetComponent(out AudioSource audioSource) && EatSound != null)
+        {
+            audioSource.pitch = Random.Range(0.9f, 1.1f);
+            audioSource.PlayOneShot(EatSound);
         }
     }
 }

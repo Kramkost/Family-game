24faa3438c59e3266c87825b32eeb1baa92da64e
Mixup <Shevmisha03@gmail.com>
2026-04-11 20:52:34 +0300

    Fix

---
+++ Assets/Scenes/Absolute Menu/Menu/LobbyDataEntry.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.UI;
+using Steamworks;
+using TMPro;
+
+public class LobbyDataEntry : MonoBehaviour
+{
+    //Data
+    public CSteamID lobbyID;
+    public string lobbyName;
+    public TMP_Text lobbyNameText;
+
+    public void SetLobbyData()
+    {
+        if (lobbyName == "")
+        {
+            lobbyNameText.text = "Empty";
+        }
+        else
+        {
+            lobbyNameText.text = lobbyName;
+        }
+    }
+
+    public void JoinLobby()
+    {
+        SteamLobby.JoinLobby(lobbyID);
+    }
+}
